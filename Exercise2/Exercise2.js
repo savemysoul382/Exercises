79f4b441fdd9a1ae12b42fee2810f1e2c61b2262
savemysoul382 <serjke88@gmail.com>
2017-08-04 18:34:39 +0300

    another check

--- Exercise2/Exercise2.js
+++ Exercise2/Exercise2.js
@@ -1,7 +1,8 @@
 var passengers = [  { name: "Jane Doloop", paid: true},
-    { name: "Dr. Evel", paid: true,},
-    { name: "Sue Property", paid: false},
-    { name: "John Funcall", paid: true} ];
+                    { name: "Dr. Evel", paid: true,},
+                    { name: "Sue Property", paid: false},
+                    { name: "John Funcall", paid: true}
+];
 
 function checkPaid(passengers) {
     for (var i = 0; i < passengers.length; i++) {
@@ -28,4 +29,47 @@ function printPassengers(passengers) {
     }
     return true;
 }
+
+function processPassengers(passengers, testFunction) {
+    for (var i = 0; i < passengers.length; i++) {
+        if (testFunction(passengers[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkNoFlyList(passenger) {
+    return (passenger.name === "Dr. Evil");
+}
+
+function checkNotPaid(passenger) {
+    return(!passenger.paid);
+}
+
+//2 ex
+var allCanFly = processPassengers (passengers, checkNoFlyList);
+if (!allCanFly) {
+    console.log("The plane can`t take off, we have a passenger ");
+}
+
+var allPaid = processPassengers (passengers, checkNotPaid);
+if (!allPaid) {
+    console.log("The plane can`t take off: not everyone has paid.")
+}
+
+//3 ex
+
+function printPassenger(passenger) {
+    var message = passenger.name;
+    if (passenger.paid === true) {
+        message = message + " has paid";
+    } else {
+        message = message + " has not paid";
+    }
+    console.log(message);
+    return false;
+}
+
+processPassengers(passengers, printPassenger);
 
